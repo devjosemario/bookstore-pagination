d6b9ddb659c4ed8444512f3af0565ba57b21b9c5
devjosemario <joosemariovm@gmail.com>
2026-05-25 17:53:44 -0300

    Revert "feat: adiciona paginacao ao DRF"
    This reverts commit 80d19b0098f2e4122428e48239ec8fddd31781e2.

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -142,13 +142,13 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # REST Framework settings
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-    ],
+'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+'PAGE_SIZE': 10,
+'DEFAULT_AUTHENTICATION_CLASSES': [
+'rest_framework.authentication.BasicAuthentication',
+'rest_framework.authentication.SessionAuthentication',
+'rest_framework.authentication.TokenAuthentication',
+],
 }
 
 # Debug toolbar settings
